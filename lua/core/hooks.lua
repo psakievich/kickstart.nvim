4def04bc954553e4b7cd422777935b5172f00c89
psakiev <psakiev@sandia.gov>
2023-12-11 06:23:55 -0700

    Make vim commands for modes

--- lua/core/hooks.lua
+++ lua/core/hooks.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
   pattern = {"*note-*.md"},
   command = "silent !buildnote %:p",
 })
-
+--
 -- hooks for switching modes from personal dev to pairing and back
 develop_mode = function()
   vim.wo.relativenumber = true
@@ -13,10 +13,12 @@ develop_mode = function()
   vim.wo.cursorline = false
   vim.o.hlsearch = false
 end
-
+vim.api.nvim_create_user_command("SoloMode", "lua develop_mode()", {})
+--
 pairing_mode = function()
   vim.wo.relativenumber = false
   vim.wo.number = true
   vim.wo.cursorline = true
   vim.o.hlsearch = true
 end
+vim.api.nvim_create_user_command("PairingMode", "lua pairing_mode()", {})
